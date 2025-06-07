fbef7be1f1451a75d218d3b04de186bfa0f188cd
Роман Беспалов <bespalovroma1995@gmail.com>
2025-06-07 19:37:42 +0300

    added_logging

--- bot/settings_logs.py
+++ bot/settings_logs.py
@@ -2,7 +2,7 @@ import logging
 
 logger = logging.getLogger("chat_logger")
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler("/app/logs/chat.log", mode="a", encoding="utf-8")
+handler = logging.FileHandler("./chat.log", mode="a", encoding="utf-8")
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
